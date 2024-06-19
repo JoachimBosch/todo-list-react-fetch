9405cf3f3856f5230fae5ae5d27050ce42b270d2
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-06-19 14:32:48 +0200

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,89 @@
-import { useState } from 'react'
-import './index.css'
+import { useState, useEffect } from 'react'
+import './index.css';
 
-function App(props) {
-  const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+function App() {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
 
-  const handleInput = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleKeyUp = (e) => {
-    if (e.keyCode === 13 && inputValue.trim() !== '') {
-      const newItem = { id: Date.now(), value: inputValue}
-      setItems([...items, newItem]);
-      setInputValue("");
+  async function fetchTasks() {
+    try {
+      const response = await fetch('https://playground.4geeks.com/todo/users/joachimbosch')
+      const apiList = await response.json();
+      /* console.log(apiList.todos); */
+      setList(apiList.todos);
+    }
+    catch (error) {
+      console.error('Error fetching tasklist:', error)
     }
   };
 
-  const handleDelete = (id) => {
-    setItems(items.filter(item => item.id !== id));
+  useEffect(() => {
+    fetchTasks()
+  }, []);
+
+	const addTask = async (e) => {
+    if(e.key === 'Enter' && task.trim() !== "") {
+      const addTaskToAPI = await fetch ("https://playground.4geeks.com/todo/todos/joachimbosch", {
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({label: task, is_done: false})
+      });
+      
+      fetchTasks();
+      setTask("");
+      console.log(list)
+      }
   };
+
+  const deleteTask = async (index) => {
+    let deleteTaskAPI = await fetch(`https://playground.4geeks.com/todo/todos/${list[index].id}`, {
+      method: "DELETE"
+    });
+    fetchTasks()
+  };
+
+  const deleteAll = async () => {
+    const deleteAll = list.map(item => fetch(`https://playground.4geeks.com/todo/todos/${item.id}`, {
+      method: "DELETE"
+    })
+  );
+
+  await Promise.all(deleteAll);
+  setList([]);
+  }
+ 
 
   return (
     <div className="wrapper">
       <div className="list">
       <div className="list-header">
-        <h1>TODOS</h1>
+        <h1>Daily tasks:</h1>
       </div>
       <div className="input-area">
-        <input className="inputItem" type="text" placeholder="What needs to be done?" value={inputValue} onChange={handleInput} onKeyUp={handleKeyUp}>
+        <input className="inputItem" type="text" placeholder="What needs to be done?" value={task} onChange={(e) => setTask(e.target.value)} onKeyUp={addTask}>
           </input>
       </div>
       <div className="item">
         <div>
           <ul>
-          {items && items.length < 1
+          {list && list.length < 1
           ? 
           <li key="0">
-          No current tasks, add a task and get to work.
+          No current tasks, are you lazy or bored?
           </li>
-          : items.map((item) => (
-              <li key={item.id} className="task-item">
-                {item.value}
-                <button className="delete-button" onClick={() => handleDelete(item.id)}>X</button>
+          : list.map((item, index) => (
+              <li key={index} className="task-item">
+                {index + 1}. {item.label}
+                <button className="delete-button" onClick={() => deleteTask(index)}>X</button>
               </li>
             ))}
           </ul>
         </div>
         <div className="counter">
-        {items.length === 1 ? '1 item left' : `${items.length} items left`}
+        {list.length === 1 ? '1 item left' : `${list.length} items left`}
         </div>
       </div>
-
+        <button className="deleteAll" onClick={() => deleteAll()}>Delete all tasks</button>
 
 
 
@@ -59,7 +91,7 @@ function App(props) {
       <div className="page1"> </div>
       <div className="page2"> </div>
     </div>
-  )
-}
+  )};
+
 
 export default App
